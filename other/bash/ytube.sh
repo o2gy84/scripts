2829b685fa48a649e03ebd841cf13e680599fa00
Victor Mogilin <o2gy84@gmail.com>
2015-06-21 13:45:40 -0700

    ytube.sh added --playlist-start option

--- other/bash/ytube.sh
+++ other/bash/ytube.sh
@@ -10,5 +10,5 @@ echo "grab: $1"
 user="imap.test.13@gmail.com"
 pass="imaptest13"
 
-youtube-dl --extract-audio --audio-format mp3 -u $user -p $pass $1
+youtube-dl --extract-audio --audio-format mp3 -u $user -p $pass $1 --playlist-start $2
 
